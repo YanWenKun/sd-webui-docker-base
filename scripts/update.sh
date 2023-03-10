05fa28551096dabd15840fd4e8d0414dd4eab47d
YAN Wenkun <code@yanwk.fun>
2023-03-10 17:54:59 +0800

    Speed up updating & bugfix

--- scripts/update.sh
+++ scripts/update.sh
@@ -12,12 +12,14 @@ echo "########################################"
 echo "Updating extensions..."
 echo "########################################"
 
+# '&' will run command in background, effectively parallel.
 cd "/home/runner/stable-diffusion-webui/extensions" &&
 for D in *; do
     if [ -d "${D}" ]; then
-        echo "${D}" && git pull
+        git -C "${D}" pull && echo "Done: ${D}" &
     fi
 done
+wait $(jobs -p)
 
 echo "Update complete."
 exit 0
