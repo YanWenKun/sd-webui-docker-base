24269988297312a398428913e7d1392efc969ffb
YAN Wenkun <code@yanwk.fun>
2024-03-04 01:26:26 +0800

    fix alias
    fix #1

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-alias gcs='git clone --depth=1 --no-tags --recurse-submodules --shallow-submodules'
+gcs='git clone --depth=1 --no-tags --recurse-submodules --shallow-submodules'
 
 echo "########################################"
 echo "Downloading SD-WebUI & components..."
@@ -9,40 +9,40 @@ echo "########################################"
 set -euxo pipefail
 
 cd /home/runner
-gcs https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
+$gcs https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
 
 # Download latest dependency repos.
 # Another crazy move but still works.
 mkdir -p /home/runner/stable-diffusion-webui/repositories
 cd /home/runner/stable-diffusion-webui/repositories
 
-gcs https://github.com/Stability-AI/stablediffusion.git \
+$gcs https://github.com/Stability-AI/stablediffusion.git \
     stable-diffusion-stability-ai
-gcs https://github.com/Stability-AI/generative-models.git \
+$gcs https://github.com/Stability-AI/generative-models.git \
     generative-models
-gcs https://github.com/CompVis/taming-transformers.git \
+$gcs https://github.com/CompVis/taming-transformers.git \
     taming-transformers
-gcs https://github.com/crowsonkb/k-diffusion.git \
+$gcs https://github.com/crowsonkb/k-diffusion.git \
     k-diffusion
-gcs https://github.com/sczhou/CodeFormer.git \
+$gcs https://github.com/sczhou/CodeFormer.git \
     CodeFormer
-gcs https://github.com/salesforce/BLIP.git \
+$gcs https://github.com/salesforce/BLIP.git \
     BLIP
 
 mkdir -p /home/runner/stable-diffusion-webui/extensions
 cd /home/runner/stable-diffusion-webui/extensions
 
-gcs https://github.com/Mikubill/sd-webui-controlnet.git \
+$gcs https://github.com/Mikubill/sd-webui-controlnet.git \
     controlnet
-gcs https://github.com/nonnonstop/sd-webui-3d-open-pose-editor.git \
+$gcs https://github.com/nonnonstop/sd-webui-3d-open-pose-editor.git \
     3d-open-pose-editor
-gcs https://github.com/AlUlkesh/stable-diffusion-webui-images-browser.git \
+$gcs https://github.com/AlUlkesh/stable-diffusion-webui-images-browser.git \
     images-browser
-gcs https://github.com/DominikDoom/a1111-sd-webui-tagcomplete.git \
+$gcs https://github.com/DominikDoom/a1111-sd-webui-tagcomplete.git \
     tag-autocomplete
-gcs https://github.com/toshiaki1729/stable-diffusion-webui-text2prompt.git \
+$gcs https://github.com/toshiaki1729/stable-diffusion-webui-text2prompt.git \
     text2prompt
-gcs https://github.com/dtlnor/stable-diffusion-webui-localization-zh_CN.git \
+$gcs https://github.com/dtlnor/stable-diffusion-webui-localization-zh_CN.git \
     localization-zh_CN
 
 cd /home/runner/stable-diffusion-webui
